d616f5f0bb97527041bb093f844f4fa2a12bcf0f
Ali Mohatasim 0060 <ali.mohatasim0060@stud.hkr.se>
2026-05-13 16:43:32 +0200

    Move database files into correct server backend

--- server/models/User.js
+++ server/models/User.js
@@ -2,21 +2,30 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    username: {
+    name: {
       type: String,
       required: true,
+      trim: true,
     },
 
     email: {
       type: String,
       required: true,
       unique: true,
+      lowercase: true,
     },
 
     password: {
       type: String,
       required: true,
     },
+
+    favorites: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Recipe",
+      },
+    ],
   },
   { timestamps: true }
 );
